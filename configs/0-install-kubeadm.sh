a217b52aa2dc6bcb1f6e70f0b74c05317c69e907
Ivan Sharamok <isharamok@gmail.com>
2021-02-05 18:48:09 -0800

    update install script

--- configs/0-install-kubeadm.sh
+++ configs/0-install-kubeadm.sh
@@ -18,6 +18,7 @@ sudo apt-mark hold kubelet kubeadm kubectl
 # Installing Docker CE
 sudo curl -fSsl https://get.docker.com | sh
 sudo usermod -aG docker ubuntu
+newgrp docker
 
 # Installing calicoctl
 sudo curl -O -L  https://github.com/projectcalico/calicoctl/releases/download/v$CALICO_VERSION/calicoctl && sudo chmod +x calicoctl && sudo mv calicoctl /usr/bin
