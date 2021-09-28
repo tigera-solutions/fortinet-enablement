b5eb6a88d29c6bc0241b4dd9add48a2d176ba55c
Ivan Sharamok <isharamok@gmail.com>
2021-09-28 13:15:43 -0700

    update k8s and calico versions

--- configs/0-install-kubeadm.sh
+++ configs/0-install-kubeadm.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-KUBERNETES_VERSION=1.21.4-00
-CALICO_VERSION=3.9.0
+KUBERNETES_VERSION=1.21.5-00
+CALICO_VERSION=3.10.0
 
 # Installing all required packages to run Kubeadm
 sudo apt-get update && sudo apt-get install -y apt-transport-https curl
@@ -23,4 +23,10 @@ newgrp docker
 # Installing calicoctl
 sudo curl -O -L  https://downloads.tigera.io/ee/binaries/v$CALICO_VERSION/calicoctl && sudo chmod +x calicoctl && sudo mv calicoctl /usr/bin
 export CALICO_DATASTORE_TYPE=kubernetes
-export CALICO_KUBECONFIG=~/.kube/config 
+export CALICO_KUBECONFIG=~/.kube/config
+
+# Configure kubectl autocomplete
+echo 'source <(kubectl completion bash)' >>~/.bashrc
+echo 'alias k=kubectl' >>~/.bashrc
+echo 'complete -F __start_kubectl k' >>~/.bashrc
+source ~/.bashrc
